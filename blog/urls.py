e326879c7e8ce9aed9e7e397648534b1dc7569f6
Loveleen Kaur <lkaur21@basecampcodingacademy21.com>
2021-01-13 22:07:40 -0600

    chapter 5 commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from .views import BlogListView, BlogDetailView
-urlpatterns=[
-    path('post/<int:pk>/', BlogDetailView.as_view(),
-    name='post_detail'),
-    path('', BlogListView.as_view(), name="home"),
-]
+from django.urls import path 
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView, 
+    BlogDeleteView, # new
+    )
+urlpatterns =[
+    path('post/<int:pk>/delete/',BlogDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit/',BlogUpdateView.as_view(), name='post_edit'), # new
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('', BlogListView.as_view(), name='home'),
+    ]
